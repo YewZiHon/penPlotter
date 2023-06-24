a8c3762d4bd95a4743284fce0aa8cfb7c91ea81c
YewZiHon <82492224+YewZiHon@users.noreply.github.com>
2023-06-24 20:27:02 +0800

    changed images --ignore

--- img_to_gc/svgtoGcode.py
+++ img_to_gc/svgtoGcode.py
@@ -45,12 +45,13 @@ class CustomInterface(interfaces.Gcode):
         return "G28"
 
 
-# Instantiate a compiler, specifying the interface type and the speed at which the tool should move. pass_depth controls
-# how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
-gcode_compiler = Compiler(CustomInterface, movement_speed=1000, cutting_speed=300, pass_depth=5)
+
 
 def gcodeGenerate(path):
-    global gcode_compiler
+    # Instantiate a compiler, specifying the interface type and the speed at which the tool should move. pass_depth controls
+    # how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
+    gcode_compiler = Compiler(CustomInterface, movement_speed=1000, cutting_speed=300, pass_depth=5)
     curves = parse_file(sys.path[0]+"\\rufus.svg") # Parse an svg file into geometric curves
     gcode_compiler.append_curves(curves) 
     gcode_compiler.compile_to_file(path, passes=1)
+    del gcode_compiler
